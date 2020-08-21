b8a390f9d9583f9b9b48170c243038838efb6768
Marcus Thorne <marcus_thorne475@hotmail.com>
2020-08-21 13:40:48 +0100

    fixed upo some things and added the rest of the filter

--- app/models/art.rb
+++ app/models/art.rb
@@ -3,6 +3,10 @@ class Art < ApplicationRecord
   # validates :name, :style, :price, :description, :photo, :artist, presence: true
   has_one_attached :photo
 
+  enum style: [ :abstract, :fine_art, :modern, :figurative, :abstract_expressionism, :expressionism, :realism, :conceptual_impressionism, :surrealism, :portraiture, :pop_art, :minimalism, :illustration, :street_art, :documentary, :art_deco, :photorealism, :folk, :cubism, :dada ]
+  enum category: [ :paintings, :photogrpahy, :drawings, :new_media, :sculpture, :collage, :printmaking, :installation, :video ]
+  enum subject: [ :landscape, :people, :portrait, :nature, :animal, :women, :floral, :still_life, :nude, :fantasy, :architecture, :seascape, :cities, :body, :love, :botanic, :world_culture, :beach, :culture, :geometric, :pop_culture, :erotic, :places, :religious, :music ]
+
   include PgSearch::Model
   pg_search_scope :search_by_title_and_artist,
     against: [ :name, :artist ],
